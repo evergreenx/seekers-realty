a0866048802a67c3a38b482e05938e96f91ae414
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-02-28 23:53:01 +0100

    fixing selects

--- components/Filter/index.tsx
+++ components/Filter/index.tsx
@@ -5,13 +5,23 @@ import Range from "../Filter/Range/index";
 
 const Index = () => {
   const [selectedOption, setSelectedOption] = React.useState(null);
+  const [purposeValue , setPurposeValue] = React.useState("");
+ const [sortValue , setSortValue] = React.useState("");
 
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
+
+
+  const purposeOptions = [
+    { value: "buy", label: "buy" },
+    { value: "rent", label: "rent" },
+    
   ];
 
+
+  // const sortOptions = [
+  //   { value: "buy", label: "buy" },
+  //   { value: "rent", label: "rent" },
+    
+  // ];
   return (
     <div className="bg-[#F3F3FA] my-10 ">
       <div className="filter__container max-w-5xl mx-auto p-5  py-10  ">
@@ -23,41 +33,41 @@ const Index = () => {
         <div className="grid grid-cols-1 lg:grid-cols-4 px-10 py-10    gap-5">
         <Select
           value={selectedOption}
-          onChange={() => {
-            setSelectedOption(selectedOption);
-          }}
-          options={options}
+          onChange={(option) => setSelectedOption(option)}
+        
+          
+          placeholder="Purpose"
+          options={purposeOptions}
           className="border border-stone-900 rounded"
 
         />
 
 <Select
           value={selectedOption}
-          onChange={() => {
-            setSelectedOption(selectedOption);
-          }}
-          options={options}
+          onChange={(option) => setSelectedOption(option)}
+
+          placeholder={"Sort By"}
+
+          options={purposeOptions}
           className="border border-stone-900 rounded"
         />
 
 
 <Select
           value={selectedOption}
-          onChange={() => {
-            setSelectedOption(selectedOption);
-          }}
-          options={options}
+          onChange={(option) => setSelectedOption(option)}
+         
+          options={purposeOptions}
 
           className="border border-stone-900 rounded"
           
         /> <Select
         value={selectedOption}
-        onChange={() => {
-          setSelectedOption(selectedOption);
-        }}
+        onChange={(option) => setSelectedOption(option)}
+       
         className="border border-stone-900 rounded"
 
-        options={options}
+        options={purposeOptions}
       />
       </div>
 
